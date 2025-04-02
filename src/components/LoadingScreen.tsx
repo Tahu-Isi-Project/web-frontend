f156661b5b61e254494dfaf2dc42a1a50378e776
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2025-04-02 14:54:24 +0700

    📦 add loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,11 +1,59 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { cn, sleep } from "../utils";
 
-export default function LoadingScreen(): ReactNode {
+export interface FlavorText {
+  time: number,
+  text: string,
+};
+
+export interface LoadingScreenProps {
+  className?: string,
+  flavorText: FlavorText[]
+};
+
+export default function LoadingScreen(props: LoadingScreenProps): ReactNode {
+  const [shownContent, setShowContent] = useState<FlavorText[]>([]);
+  const increment = 100 / (props.flavorText.length + 1);
+  const [loadingLength, setLoadingLength] = useState(increment);
+  const hasLoaded = useRef(false);
+
+  useEffect(() => {
+    async function load() {
+      if (hasLoaded.current) return;
+      hasLoaded.current = true;
+
+      for (const text of props.flavorText) {
+        setShowContent(prev => [...prev, text]);
+        setLoadingLength(prev => prev + 1);
+        await sleep(Math.random() * text.time);
+      }
+    }
+    load();
+  }, []);
+
   return (
-    <div className="relative w-screen h-screen flex justify-center items-center dark:bg-slate-950">
+    <div className={cn(
+      "absolute z-100 w-screen h-screen flex flex-col justify-center items-center dark:bg-slate-950 duration-500",
+      props.className,
+    )}>
       <h2 className="font-bold text-3xl dark:text-white shadow-white/50 shadow-text">
         Please wait warmly
       </h2>
+
+      <div className="relative min-h-[48vh] w-[24vw] sm:w-[82vw] md:w-[60vw] lg:w-[42vw] flex flex-col-reverse items-start justify-center overflow-hidden dark:text-white">
+        {shownContent.map((content) =>
+          <span key={content.text} className="font-bold">
+            [{
+              "\u00A0".repeat(
+                5 - content.time.toString().length >= 0
+                  ? 5 - content.time.toString().length
+                  : 0,
+              ) + content.time
+            }] {content.text}
+          </span>
+        )}
+      </div>
+
       <div
         className="absolute inset-0 z-10 w-screen bg-gradient-to-b from-transparent from-40% to-black to-80%"
       ></div>
--- src/lib/Loader.tsx
+++ src/lib/Loader.tsx
@@ -1,12 +1,51 @@
-import { PropsWithChildren, ReactNode, Suspense } from "react";
+import { PropsWithChildren, ReactNode, Suspense, useEffect, useState } from "react";
 import LoadingScreen from "@/components/LoadingScreen";
+import { FlavorText } from "../components/LoadingScreen";
+import { sleep } from "../utils";
 
 // TODO : implement proper loading in React (probably need implement custom router)
 type LoaderProps = PropsWithChildren;
+
+interface LoaderTrigger {
+  done: () => void;
+}
+
+function LoaderTrigger(props: LoaderTrigger): ReactNode {
+  useEffect(() => {
+    return () => {
+      props.done();
+    }
+  }, []);
+
+  return (<></>);
+}
+
+
 export default function Loader(props: LoaderProps): ReactNode {
+  const [done, setDone] = useState(false);
+
+  const flavorText = [
+    { time: 0, text: "Booting" },
+    { time: 1, text: "Load minimal resources" },
+    { time: 12, text: "Loading assets" },
+    { time: 24, text: "Loading tailwind" },
+    { time: 42, text: "Loading content" },
+    { time: 52, text: "Content loaded" },
+    { time: 72, text: "Starting app" },
+    { time: 102, text: "Finishing" },
+  ] as FlavorText[];
+
+  async function loadDone() {
+    await sleep(300);
+    setDone(() => true);
+  }
+
   return (
-    <Suspense fallback={<LoadingScreen />}>
-      {props.children}
-    </Suspense>
+    <>
+      <LoadingScreen className={done ? 'opacity-0' : ''} flavorText={flavorText} />
+      <Suspense fallback={<LoaderTrigger done={loadDone} />}>
+        {props.children}
+      </Suspense>
+    </>
   );
 }
